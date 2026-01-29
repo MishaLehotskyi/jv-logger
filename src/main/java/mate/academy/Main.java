a91990a7dcebd0cce271a8703a39eee5be071f11
MishaLehotskyi <eglet777@ukr.net>
2026-01-29 22:05:03 +0100

    added task solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,5 +1,8 @@
 package mate.academy;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
 import mate.academy.service.AuthenticationService;
@@ -8,16 +11,27 @@ import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
+        logger.info("Application started");
+
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
+
         try {
+            logger.info("Attempting to authenticate user: bob");
             user = authenticationService.login("bob", "1234");
+            logger.info("User authenticated successfully: {}", user.getLogin());
         } catch (AuthenticationException e) {
-            e.printStackTrace();
+            logger.error("Authentication failed for user: bob", e);
             return;
         }
+
+        logger.info("Proceeding to order completion for userId={}", user.getUserId());
         OrderService orderService = new OrderServiceImpl();
         orderService.completeOrder(user.getUserId());
+
+        logger.info("Application finished successfully");
     }
 }
--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -1,23 +1,36 @@
 package mate.academy.service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        //TODO: add corresponding log message about method login was called
+        logger.info("Method login was called. Params: login={}", login);
+
         User user = findByLogin(login);
+
         if (!user.getPassword().equals(password)) {
             throw new AuthenticationException("Username or password are incorrect");
         }
+
+        logger.info("User {} successfully authenticated", login);
         return user;
     }
 
     private User findByLogin(String login) {
+        logger.info("Looking up user by login: {}", login);
+
         User user = new User(login, "1234");
         // this user identifier should be set by DB. We will use dummy data for this example
         user.setUserId(2L);
+
+        logger.info("User found: userId={}, login={}", user.getUserId(), login);
         return user;
     }
 }
